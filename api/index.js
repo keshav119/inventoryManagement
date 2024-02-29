13917ecc6562cf055f6423c3d02391a526de030f
keshav119 <channakeshav99.ck@gmail.com>
2024-02-29 19:34:29 +0530

    initial setup of React and Node JS

---
+++ api/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+const cors = require("cors");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("error"));
+  
+  app.use(cors());
+  app.use(express.json());
+  
+  app.use("/api/auth", authRoute);
+  app.use("/api/users", userRoute);
+
+app.listen(3000, () => {
+    console.log("Backend is running");
+});
+
+module.exports = app;
